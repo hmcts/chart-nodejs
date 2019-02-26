3d124a8b2a57c977584ef8d4948e0de42acfe7b1
Kevin Cross <kevin.j.cross@gmail.com>
2019-02-26 16:01:29 +0000

    Feature/properties module (#17)

--- test-image/index.js
+++ test-image/index.js
@@ -1,25 +1,21 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.NODE_PORT;
-const fsPromise = require('fs').promises;
-const payload = { message: "Im all right !" };
-const expectedfiles=[
-  "/mnt/secrets/bulk-scan/idam-client-secret",
-  "/mnt/secrets/bulk-scan/s2s-secret",
-  "/mnt/secrets/s2s/microservicekey-ccd-admin",
-  "/mnt/secrets/s2s/microservicekey-ccd-data",
-]
+const express = require('express')
+const app = express()
+const payload = {message: 'Im all right !'}
+const config = require('@hmcts/properties-volume').addTo(require('config'))
+const port = config.get('server.port')
 
-
-app
-  .get("/", (req, res) => res.send(payload))
-  .get("/health",  (req, res) => {
-    files = expectedfiles.map( file => fsPromise.open(file, 'r' ))
-    Promise.all(files)
-    .then( files =>  res.send(payload) )  
-    .catch( err => {
-      console.log(`ERROR:`+err);
-      res.sendStatus(500);
-    });
+app.get('/', (req, res) => res.send(payload))
+  .get('/health/liveness', (req, res) =>  res.send(payload) )
+  .get('/health', (req, res) => {
+    try {
+      config.get('secrets.bulk-scan.idam-client-secret')
+      config.get('secrets.bulk-scan.s2s-secret')
+      config.get('secrets.s2s.microservicekey-ccd-admin')
+      config.get('secrets.s2s.microservicekey-ccd-data')
+      res.send(payload)
+    } catch (error) {
+      console.log(`ERROR:` + error)
+      res.sendStatus(500)
+    }
   })
-  .listen(PORT, () => console.log(`App listenning on http://0.0.0.0:${PORT}`));
+  .listen(port, () => console.log(`chart-nodeJs test app listening on http://0.0.0.0:${port}`))
