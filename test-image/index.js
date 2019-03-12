6c55a8fa41cd7cbd30cdcf24ce4bbc8812ba38e7
Kevin Cross <kevin.j.cross@gmail.com>
2019-03-12 09:36:43 +0000

    Addition of the healthcheck module (#20)
    * update reference to version "1.5.1" of "nodejs-healthcheck"
    
    * update livenessPath
    
    * Rerun CI
    
    *update tests

--- test-image/index.js
+++ test-image/index.js
@@ -4,18 +4,30 @@ const payload = {message: 'Im all right !'}
 const config = require('@hmcts/properties-volume').addTo(require('config'))
 const port = config.get('server.port')
 
-app.get('/', (req, res) => res.send(payload))
-  .get('/health/liveness', (req, res) =>  res.send(payload) )
-  .get('/health', (req, res) => {
-    try {
-      config.get('secrets.bulk-scan.idam-client-secret')
-      config.get('secrets.bulk-scan.s2s-secret')
-      config.get('secrets.s2s.microservicekey-ccd-admin')
-      config.get('secrets.s2s.microservicekey-ccd-data')
-      res.send(payload)
-    } catch (error) {
-      console.log(`ERROR:` + error)
-      res.sendStatus(500)
+const healthcheck = require('@hmcts/nodejs-healthcheck')
+healthcheck.addTo(app,
+  {
+    checks: {
+      secretsCheck: healthcheck.raw(() => checkForSecrets() ? healthcheck.up() : healthcheck.down())
+    },
+    buildInfo: {
+      'chart-testing': 'nodejs-chart test'
     }
   })
+
+function checkForSecrets() {
+  try {
+    config.get('secrets.bulk-scan.idam-client-secret')
+    config.get('secrets.bulk-scan.s2s-secret')
+    config.get('secrets.s2s.microservicekey-ccd-admin')
+    config.get('secrets.s2s.microservicekey-ccd-data')
+    return true
+  } catch (error) {
+    console.log(`ERROR:` + error)
+
+    return false
+  }
+}
+
+app.get('/', (req, res) => res.send(payload))
   .listen(port, () => console.log(`chart-nodeJs test app listening on http://0.0.0.0:${port}`))
