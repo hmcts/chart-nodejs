125fdda913e3087f7d108ea86dcb94c771abc23f
Marty Fox <martin.fox1@justice.gov.uk>
2023-11-30 15:55:14 +0000

    Testing test image with webcheck not secrets function

--- test-image/index.js
+++ test-image/index.js
@@ -8,24 +8,12 @@ const healthcheck = require('@hmcts/nodejs-healthcheck')
 healthcheck.addTo(app,
   {
     checks: {
-      secretsCheck: healthcheck.raw(() => checkForSecrets() ? healthcheck.up() : healthcheck.down())
+      webCheck: healthcheck.web("https://0.0.0.0:${port}/health"),
     },
     buildInfo: {
       'chart-testing': 'nodejs-chart test'
     }
   })
-
-function checkForSecrets() {
-  try {
-    config.get('secrets.bulk-scan.idam-client-secret')
-    config.get('secrets.bulk-scan.s2s-secret')
-    return true
-  } catch (error) {
-    console.log(`ERROR:` + error)
-
-    return false
-  }
-}
 
 app.get('/', (req, res) => res.send(payload))
   .listen(port, () => console.log(`chart-nodeJs test app listening on http://0.0.0.0:${port}`))
